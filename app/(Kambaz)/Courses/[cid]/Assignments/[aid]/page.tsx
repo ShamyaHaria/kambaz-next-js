fd7126aef4c648615f25f71c485d786003d2edad
Shamya <shamyaharia@gmail.com>
2025-09-18 14:11:44 -0400

    Assignment Create Features

--- app/(Kambaz)/Courses/[cid]/Assignments/[aid]/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/[aid]/page.tsx
@@ -16,7 +16,41 @@ export default function AssignmentEditor() {
                         <input id="wd-points" value={100} />
                     </td>
                 </tr>
-                {/* Complete on your own */}
+                <tr>
+                    <td align="right" valign="top">
+                        <label htmlFor="wd-due">Due Date</label>
+                    </td>
+                    <td>
+                        <input type="date" id="wd-due" />
+                    </td>
+                </tr>
+                <tr>
+                    <td align="right" valign="top">
+                        <label htmlFor="wd-type">Assignment Type</label>
+                    </td>
+                    <td>
+                        <select id="wd-type" defaultValue="Homework">
+                            <option>Homework</option>
+                            <option>Project</option>
+                            <option>Quiz</option>
+                            <option>Exam</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td align="right" valign="top">
+                        <label htmlFor="wd-graded">Graded?</label>
+                    </td>
+                    <td>
+                        <input type="checkbox" id="wd-graded" defaultChecked />
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <button type="button">Save Assignment</button>
+                    </td>
+                </tr>
             </table>
         </div>
     );
