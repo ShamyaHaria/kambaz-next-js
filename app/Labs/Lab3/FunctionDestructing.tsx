1421ecbc5ce5fa2bc8256a613e2342606815dc86
Shamya <shamyaharia@gmail.com>
2025-10-11 15:37:33 -0400

    Update FunctionDestructing.tsx

--- app/Labs/Lab3/FunctionDestructing.tsx
+++ app/Labs/Lab3/FunctionDestructing.tsx
@@ -1,23 +1,18 @@
-import React from "react";
-import Math, { add, subtract, multiply, divide } from "./Math";
-import * as Matematica from "./Math";
-export default function DestructingImports() {
-  return (
-    <div id="wd-destructuring-imports">
-      <h2>Destructing Imports</h2>
-      <table className="table table-sm">
-        <thead>
-          <tr>
-            <th>Math</th>
-            <th>Matematica</th>
-            <th>Functions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* see next code block */}
-        </tbody>
-      </table>
-      <hr />
-    </div>
-);}
+export default function FunctionDestructing() {
+    const add = (a: number, b: number) => a + b;
+    const sum = add(1, 2);
+    const subtract = ({ a, b }: { a: number; b: number }) => a - b;
+    const difference = subtract({ a: 4, b: 2 });
+    return (
+        <div id="wd-function-destructing">
+            <h2>Function Destructing</h2>
+            const add = (a, b) =&gt; a + b;<br />
+            const sum = add(1, 2);<br />
+            const subtract = (&#123; a, b &#125;) =&gt; a - b;<br />
+            const difference = subtract(&#123; a: 4, b: 2 &#125;);<br />
+            sum = {sum}<br />
+            difference = {difference} <hr />
+        </div>
+    );
+}
 
