dab259a01789b5621ac6f258ead412b0c226c1b6
Shamya <shamyaharia@gmail.com>
2025-10-16 19:47:39 -0400

    Databases

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -1,23 +1,24 @@
 import { ReactNode } from "react";
 import CourseNavigation from "./Navigation";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { courses } from "../../Database";
 export default async function CoursesLayout(
     { children, params }: Readonly<{ children: ReactNode; params: Promise<{ cid: string }> }>) {
     const { cid } = await params;
+    const course = courses.find((course) => course._id === cid);
     return (
         <div id="wd-courses">
-            <div id="wd-courses">
-                <h2 className="text-danger">
-                    <GiHamburgerMenu className="me-4 fs-4 mb-1" />
-                    Course {cid} </h2> <hr />
-                <div className="d-flex">
-                    <div className="d-none d-md-block">
-                        <CourseNavigation />
-                    </div>
-                    <div className="flex-fill">
-                        {children}
-                    </div></div>
-            </div>
+            <h2 className="text-danger">
+                <GiHamburgerMenu className="me-4 fs-4 mb-1" />
+                {course?.name}
+            </h2>
+            <div className="d-flex">
+                <div className="d-none d-md-block">
+                    <CourseNavigation />
+                </div>
+                <div className="flex-fill">
+                    {children}
+                </div></div>
         </div>
     );
 }
--- app/(Kambaz)/Dashboard/page.tsx
+++ app/(Kambaz)/Dashboard/page.tsx
@@ -1,89 +1,32 @@
 import Link from "next/link";
 import Image from "next/image";
+import * as db from "../Database";
 import { Card, CardBody, CardImg, CardText, CardTitle, Button, Row, Col } from "react-bootstrap";
 
 export default function Dashboard() {
+    const courses = db.courses;
     return (
         <div id="wd-dashboard">
             <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-            <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
+            <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
             <div id="wd-dashboard-courses">
                 <Row xs={1} md={5} className="g-4">
+                    {courses.map((course) => (
                     <Col className="wd-dashboard-course" style={{ width: "300px" }}>
                         <Card>
-                            <Link href="/Courses/1234/Home"
+                            <Link href={`/Courses/${course._id}/Home`}
                                 className="wd-dashboard-course-link text-decoration-none text-dark">
                                 <CardImg variant="top" src="/images/reactjs.png" width="100%" height={160} />
                                 <CardBody>
-                                    <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS1234 React JS</CardTitle>
+                                    <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">{course.name}</CardTitle>
                                     <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-                                        Full Stack software developer</CardText>
+                                        {course.description} </CardText>
                                     <Button variant="primary">Go</Button>
                                 </CardBody>
                             </Link>
                         </Card>
                     </Col>
-
-                    <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-                        <Card>
-                            <Link href="/Courses/3456/Home"
-                                className="wd-dashboard-course-link text-decoration-none text-dark">
-                                <CardImg variant="top" src="/images/reactjs.png" width="100%" height={160} />
-                                <CardBody>
-                                    <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS3456 React Hooks</CardTitle>
-                                    <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-                                        Using Hooks Efficiently</CardText>
-                                    <Button variant="primary">Go</Button>
-                                </CardBody>
-                            </Link>
-                        </Card>
-                    </Col>
-
-                    <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-                        <Card>
-                            <Link href="/Courses/4567/Home"
-                                className="wd-dashboard-course-link text-decoration-none text-dark">
-                                <CardImg variant="top" src="/images/reactjs.png" width="100%" height={160} />
-                                <CardBody>
-                                    <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS4567 React State Management</CardTitle>
-                                    <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-                                        State & Effect Management</CardText>
-                                    <Button variant="primary">Go</Button>
-                                </CardBody>
-                            </Link>
-                        </Card>
-                    </Col>
-
-
-                    <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-                        <Card>
-                            <Link href="/Courses/5678/Home"
-                                className="wd-dashboard-course-link text-decoration-none text-dark">
-                                <CardImg variant="top" src="/images/reactjs.png" width="100%" height={160} />
-                                <CardBody>
-                                    <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS5678 React Performance</CardTitle>
-                                    <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-                                        Optimizing React Apps</CardText>
-                                    <Button variant="primary">Go</Button>
-                                </CardBody>
-                            </Link>
-                        </Card>
-                    </Col>
-
-                    <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-                        <Card>
-                            <Link href="/Courses/6789/Home"
-                                className="wd-dashboard-course-link text-decoration-none text-dark">
-                                <CardImg variant="top" src="/images/reactjs.png" width="100%" height={160} />
-                                <CardBody>
-                                    <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS6789 React Testing</CardTitle>
-                                    <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-                                        Testing Strategies</CardText>
-                                    <Button variant="primary">Go</Button>
-                                </CardBody>
-                            </Link>
-                        </Card>
-                    </Col>
+                    ))}
                 </Row>
             </div>
         </div>
