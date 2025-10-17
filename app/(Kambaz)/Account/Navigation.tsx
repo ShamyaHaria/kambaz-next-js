d437e77d64af30319a1be7ed3f9b97ee5ac16a16
Shamya <shamyaharia@gmail.com>
2025-10-16 20:06:01 -0400

    Temporary Account Changes

--- app/(Kambaz)/Account/Navigation.tsx
+++ app/(Kambaz)/Account/Navigation.tsx
@@ -1,15 +1,27 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import "../styles.css";
 
 export default function AccountNavigation() {
+    const pathname = usePathname();
+
     return (
         <div className="wd-account-nav">
-            <Link href="/Account/Signin" className="wd-account-nav-link wd-signin-active">
+            <Link 
+                href="/Account/Signin" 
+                className={`wd-account-nav-link ${pathname.endsWith("/Signin") ? "wd-signin-active" : ""}`}>
                 Signin
             </Link>
-            <Link href="/Account/Signup" className="wd-account-nav-link">
+            <Link 
+                href="/Account/Signup" 
+                className={`wd-account-nav-link ${pathname.endsWith("/Signup") ? "wd-signin-active" : ""}`}>
                 Signup
             </Link>
-            <Link href="/Account/Profile" className="wd-account-nav-link">
+            <Link 
+                href="/Account/Profile" 
+                className={`wd-account-nav-link ${pathname.endsWith("/Profile") ? "wd-signin-active" : ""}`}>
                 Profile
             </Link>
         </div>
