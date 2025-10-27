a98e0b90bd0228611fb53c20cf06ffb75ee4a1dd
Shamya <shamyaharia@gmail.com>
2025-10-27 15:11:26 -0400

    Event Handling - Lab4

---
+++ app/Labs/Lab4/page.tsx
@@ -0,0 +1,36 @@
+"use client"
+import React from "react";
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+
+export default function Lab4() {
+    function sayHello() {
+    alert("Hello");
+}
+    return(
+        <div id="wd-lab4">
+            <h3>Lab 4</h3>
+            <ClickEvent />
+            <PassingDataOnEvent />
+            <hr />
+            <PassingFunctions theFunction={sayHello} />
+            <EventObject />
+            <Counter />
+            <BooleanStateVariables />
+            <StringStateVariables />
+            <DateStateVariable />
+            <ObjectStateVariable />
+            <ArrayStateVariable />
+            <ParentStateComponent />
+        </div>
+    );
+}
