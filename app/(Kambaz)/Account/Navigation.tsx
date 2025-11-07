059e8a12fa618c48cecbf150956bdea8f6ec6ef3
Shamya <shamyaharia@gmail.com>
2025-11-06 21:09:22 -0500

    Profile Screen

--- app/(Kambaz)/Account/Navigation.tsx
+++ app/(Kambaz)/Account/Navigation.tsx
@@ -1,26 +1,30 @@
 "use client";
-
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import "../styles.css";
+import { Nav, NavItem, NavLink } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 export default function AccountNavigation() {
+    const { currentUser } = useSelector(
+        (state: any) => state.accountReducer);
+    const links = currentUser ? ["Profile"] :
+        ["Signin", "Signup"];
     const pathname = usePathname();
 
     return (
         <div className="wd-account-nav">
-            <Link 
-                href="/Account/Signin" 
+            <Link
+                href="/Account/Signin"
                 className={`wd-account-nav-link ${pathname.endsWith("/Signin") ? "wd-signin-active" : ""}`}>
                 Signin
             </Link>
-            <Link 
-                href="/Account/Signup" 
+            <Link
+                href="/Account/Signup"
                 className={`wd-account-nav-link ${pathname.endsWith("/Signup") ? "wd-signin-active" : ""}`}>
                 Signup
             </Link>
-            <Link 
-                href="/Account/Profile" 
+            <Link
+                href="/Account/Profile"
                 className={`wd-account-nav-link ${pathname.endsWith("/Profile") ? "wd-signin-active" : ""}`}>
                 Profile
             </Link>
--- app/(Kambaz)/Account/Profile/page.tsx
+++ app/(Kambaz)/Account/Profile/page.tsx
@@ -1,23 +1,77 @@
+"use client";
+import { redirect } from "next/dist/client/components/navigation";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setCurrentUser } from "../reducer";
 import Link from "next/link";
-import { Form } from "react-bootstrap";
+import { Button, Form, FormControl } from "react-bootstrap";
 
 export default function Profile() {
+    const { currentUser } = useSelector((state: any) => state.accountReducer);
+    const [profile, setProfile] = useState(currentUser);
+    const dispatch = useDispatch();
+    const signout = () => {
+        dispatch(setCurrentUser(null));
+        redirect("/Account/Signin");
+    };
+    const fetchProfile = () => {
+        if (!currentUser)
+            return redirect("/Account/Signin");
+        setProfile(currentUser);
+    };
+    useEffect(() => {
+        fetchProfile();
+    }, []);
+
     return (
         <div id="wd-profile-screen" className="wd-account-form">
             <h3>Profile</h3>
-            <Form.Control defaultValue="alice" placeholder="username" className="wd-username mb-2" />
-            <Form.Control defaultValue="123" placeholder="password" type="password" className="wd-password mb-2" />
-            <Form.Control defaultValue="Alice" placeholder="First Name" id="wd-firstname" className="mb-2" />
-            <Form.Control defaultValue="Wonderland" placeholder="Last Name" id="wd-lastname" className="mb-2" />
-            <Form.Control defaultValue="yyyy-mm-dd" type="date" id="wd-dob" className="mb-2" />
-            <Form.Control defaultValue="alice@wonderland" type="email" id="wd-email" className="mb-2" />
-            <Form.Select defaultValue="USER" id="wd-role" className="mb-2">
-                <option value="USER">User</option>
-                <option value="ADMIN">Admin</option>
-                <option value="FACULTY">Faculty</option>
-                <option value="STUDENT">Student</option>
-            </Form.Select>
-            <Link href="/Dashboard" className="btn btn-danger w-100">Sign out</Link>
+            {profile && (
+                <div>
+                    <FormControl id="wd-username" className="mb-2"
+                        defaultValue={profile.username}
+                        onChange={(e) => setProfile({
+                            ...profile, username: e.target.value
+                        })} />
+                    <FormControl id="wd-password" className="mb-2"
+                        defaultValue={profile.password}
+                        onChange={(e) => setProfile({
+                            ...profile, password: e.target.value
+                        })} />
+                    <FormControl id="wd-firstname" className="mb-2"
+                        defaultValue={profile.firstName}
+                        onChange={(e) => setProfile({
+                            ...profile, firstName: e.target.value
+                        })} />
+                    <FormControl id="wd-lastname" className="mb-2"
+                        defaultValue={profile.lastName}
+                        onChange={(e) => setProfile({
+                            ...profile, lastName: e.target.value
+                        })} />
+                    <FormControl id="wd-dob" className="mb-2" type="date"
+                        defaultValue={profile.dob}
+                        onChange={(e) => setProfile({
+                            ...profile, dob: e.target.value
+                        })} />
+                    <FormControl id="wd-email" className="mb-2"
+                        defaultValue={profile.email}
+                        onChange={(e) => setProfile({
+                            ...profile, email: e.target.value
+                        })} />
+                    <select className="form-control mb-2" id="wd-role"
+                        onChange={(e) => setProfile({
+                            ...profile, role: e.target.value
+                        })} >
+                        <option value="USER">User</option>
+                        <option value="ADMIN">Admin</option>
+                        <option value="FACULTY">Faculty</option>{" "}
+                        <option value="STUDENT">Student</option>
+                    </select>
+                    <Button onClick={() =>
+                        dispatch(setCurrentUser(profile))}> Save
+                    </Button>
+                </div>
+            )}
         </div>
     );
 }
