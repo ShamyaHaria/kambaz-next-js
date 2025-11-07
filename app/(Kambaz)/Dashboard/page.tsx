faa0de584ca93ef938da4be70b51cd555a4631f8
Shamya <shamyaharia@gmail.com>
2025-11-06 20:45:53 -0500

    Current User

--- app/(Kambaz)/Dashboard/page.tsx
+++ app/(Kambaz)/Dashboard/page.tsx
@@ -9,6 +9,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { addNewCourse, deleteCourse, updateCourse } from "../Courses/[cid]/reducer";
 
 export default function Dashboard() {
+    const { currentUser } = useSelector((state: any) => state.accountReducer);
+    const { enrollments } = db;
     const { courses } = useSelector((state: any) => state.coursesReducer);
     const dispatch = useDispatch();
     const [course, setCourse] = useState<any>({
@@ -42,34 +44,37 @@ export default function Dashboard() {
             <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
             <div id="wd-dashboard-courses">
                 <Row xs={1} md={5} className="g-4">
-                    {courses.map((course:any) => (
-                        <Col key={course._id} className="wd-dashboard-course" style={{ width: "300px" }}>
-                            <Card>
-                                <Link href={`/Courses/${course._id}/Home`}
-                                    className="wd-dashboard-course-link text-decoration-none text-dark">
-                                    <CardImg variant="top" src="/images/reactjs.png" width="100%" height={160} />
-                                    <CardBody>
-                                        <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">{course.name}</CardTitle>
-                                        <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-                                            {course.description} </CardText>
-                                        <Button variant="primary">Go</Button>
-                                        <Button onClick={(event) => {
+                    {courses .filter(
+                        (course:any) => enrollments.some(
+                            (enrollment) => enrollment.user === currentUser._id
+                            && enrollment.course === course._id )) .map((course: any) => (
+                    <Col key={course._id} className="wd-dashboard-course" style={{ width: "300px" }}>
+                        <Card>
+                            <Link href={`/Courses/${course._id}/Home`}
+                                className="wd-dashboard-course-link text-decoration-none text-dark">
+                                <CardImg variant="top" src="/images/reactjs.png" width="100%" height={160} />
+                                <CardBody>
+                                    <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">{course.name}</CardTitle>
+                                    <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                                        {course.description} </CardText>
+                                    <Button variant="primary">Go</Button>
+                                    <Button onClick={(event) => {
+                                        event.preventDefault();
+                                        dispatch(deleteCourse(course._id));
+                                    }} className="float-end" > Delete
+                                    </Button>
+                                    <Button id="wd-edit-course-click"
+                                        onClick={(event) => {
                                             event.preventDefault();
-                                            dispatch(deleteCourse(course._id));
-                                        }} className="float-end" > Delete
-                                        </Button>
-                                        <Button id="wd-edit-course-click"
-                                            onClick={(event) => {
-                                                event.preventDefault();
-                                                setCourse(course);
-                                            }}
-                                            className="me-2 float-end" >
-                                            Edit
-                                        </Button>
-                                    </CardBody>
-                                </Link>
-                            </Card>
-                        </Col>
+                                            setCourse(course);
+                                        }}
+                                        className="me-2 float-end" >
+                                        Edit
+                                    </Button>
+                                </CardBody>
+                            </Link>
+                        </Card>
+                    </Col>
                     ))}
                 </Row>
             </div>
--- app/(Kambaz)/store.ts
+++ app/(Kambaz)/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import coursesReducer from "./Courses/[cid]/reducer";
 import modulesReducer from "./Courses/[cid]/Modules/reducer";
+import accountReducer from "./Account/reducer";
 const store = configureStore({
   reducer: {
     coursesReducer,
     modulesReducer,
+    accountReducer,
   },
 });
 
