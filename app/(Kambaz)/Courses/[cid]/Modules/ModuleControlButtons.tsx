fa4e55e0bde9f6bc92500632eabb595234ebbba3
Shamya <shamyaharia@gmail.com>
2025-11-08 11:45:04 -0500

    Module Redux Corrected

--- app/(Kambaz)/Courses/[cid]/Modules/ModuleControlButtons.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/ModuleControlButtons.tsx
@@ -3,23 +3,12 @@ import GreenCheckmark from "./GreenCheckmark";
 import { FaTrash } from "react-icons/fa";
 import { FaPencil } from "react-icons/fa6";
 
-export default function ModuleControlButtons({ 
-    moduleId, 
-    deleteModule, 
-    editModule 
-}: {
-    moduleId?: string;
-    deleteModule?: (moduleId: string) => void;
-    editModule?: (moduleId: string) => void
-}) {
+export default function ModuleControlButtons(
+    { moduleId, deleteModule, editModule }: { moduleId: string; deleteModule: (moduleId: string) => void; editModule: (moduleId: string) => void}) {
     return (
         <div className="float-end">
-            {editModule && moduleId && (
-                <FaPencil onClick={() => editModule(moduleId)} className="text-primary me-3" />
-            )}
-            {deleteModule && moduleId && (
-                <FaTrash className="text-danger me-2 mb-1" onClick={() => deleteModule(moduleId)} />
-            )}
+            <FaPencil onClick={() => editModule(moduleId)} className="text-primary me-3" />
+            <FaTrash className="text-danger me-2 mb-1" onClick={() => deleteModule(moduleId)}/>
             <GreenCheckmark />
             <IoEllipsisVertical className="fs-4" />
         </div>
--- app/(Kambaz)/Courses/[cid]/Modules/ModulesControls.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/ModulesControls.tsx
@@ -7,51 +7,49 @@ import GreenCheckmark from "./GreenCheckmark";
 import { useState } from "react";
 
 export default function ModulesControls(
-  { moduleName, setModuleName, addModule }:
-  {
-    moduleName: string; setModuleName:
-      (title: string) => void; addModule: () => void;
-  }) {
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    return (
-      <div id="wd-modules-controls" className="text-nowrap">
-        <Button variant="danger" size="lg" className="me-1 float-end" id="wd-add-module-btn" onClick={handleShow}>
-          <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
-          Module
-        </Button>
-        <ModuleEditor dialogTitle="Add Module" moduleName={moduleName}
-                    show={show} handleClose={handleClose}
-                    setModuleName={setModuleName} addModule={addModule} />
-        <Dropdown className="float-end me-2">
-          <DropdownToggle variant="secondary" size="lg" id="wd-publish-all-btn">
+{ moduleName, setModuleName, addModule }:
+{ moduleName: string; setModuleName: (title: string) => void; addModule: () => void; }) {
+ const [show, setShow] = useState(false);
+ const handleClose = () => setShow(false);
+ const handleShow = () => setShow(true);
+  return (
+    <div id="wd-modules-controls" className="text-nowrap">
+      <Button variant="danger" onClick={handleShow}  size="lg" className="me-1 float-end" id="wd-add-module-btn"
+        data-bs-toggle="modal" data-bs-target="#wd-add-module-dialog">
+        <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
+        Module
+      </Button>
+      <Dropdown className="float-end me-2">
+        <DropdownToggle variant="secondary" size="lg" id="wd-publish-all-btn">
+          <GreenCheckmark /> Publish All
+        </DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem id="wd-publish-all">
             <GreenCheckmark /> Publish All
-          </DropdownToggle>
-          <DropdownMenu>
-            <DropdownItem id="wd-publish-all">
-              <GreenCheckmark /> Publish All
-            </DropdownItem>
-            <DropdownItem id="wd-publish-all-modules-and-items">
-              <GreenCheckmark /> Publish all modules and items
-            </DropdownItem>
-            <DropdownItem id="wd-publish-modules-only">
-              <GreenCheckmark /> Publish modules only
-            </DropdownItem>
-            <DropdownItem id="wd-unpublish-all-modules-and-items">
-              Unpublish all modules and items
-            </DropdownItem>
-            <DropdownItem id="wd-unpublish-modules-only">
-              Unpublish modules only
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-        <Button variant="secondary" size="lg" className="me-1 float-end" id="wd-view-progress">
-          View Progress
-        </Button>
-        <Button variant="secondary" size="lg" className="me-1 float-end" id="wd-collapse-all">
-          Collapse All
-        </Button>
-      </div>
-    );
-  }
+          </DropdownItem>
+          <DropdownItem id="wd-publish-all-modules-and-items">
+            <GreenCheckmark /> Publish all modules and items
+          </DropdownItem>
+          <DropdownItem id="wd-publish-modules-only">
+            <GreenCheckmark /> Publish modules only
+          </DropdownItem>
+          <DropdownItem id="wd-unpublish-all-modules-and-items">
+            Unpublish all modules and items
+          </DropdownItem>
+          <DropdownItem id="wd-unpublish-modules-only">
+            Unpublish modules only
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+      <Button variant="secondary" size="lg" className="me-1 float-end" id="wd-view-progress">
+        View Progress
+      </Button>
+      <Button variant="secondary" size="lg" className="me-1 float-end" id="wd-collapse-all">
+        Collapse All
+      </Button>
+      <ModuleEditor show={show} handleClose={handleClose} dialogTitle="Add Module"
+       moduleName={moduleName} setModuleName={setModuleName} addModule={addModule} />
+
+    </div>
+  );
+}
