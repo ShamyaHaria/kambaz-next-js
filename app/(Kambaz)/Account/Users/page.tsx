2164646c7f8d9719155e222c82e8a49f2527a081
Shamya <shamyaharia@gmail.com>
2025-12-01 23:18:16 -0500

    Users Prop Eliminated

--- app/(Kambaz)/Account/Users/page.tsx
+++ app/(Kambaz)/Account/Users/page.tsx
@@ -65,7 +65,7 @@ export default function Users() {
                 <option value="FACULTY">Faculty</option>
                 <option value="ADMIN">Administrators</option>
             </select>
-            <PeopleTable users={users} fetchUsers={fetchUsers} />
+            <PeopleTable fetchUsers={fetchUsers} />
         </div>
     );
 }
