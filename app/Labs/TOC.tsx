8fc52b26763f50c8fb4990b1c20fbda4670ec832
Shamya <shamyaharia@gmail.com>
2025-12-06 16:47:59 -0500

    Fix: Replace React-Bootstrap NavLink with Next.js Link for compatibility

--- app/Labs/TOC.tsx
+++ app/Labs/TOC.tsx
@@ -1,32 +1,35 @@
-import { Nav, NavItem, NavLink } from "react-bootstrap";
+import { Nav, NavItem } from "react-bootstrap";
 import Link from "next/link";
+
 export default function TOC() {
- return (
-   <Nav variant="pills">
-     <NavItem>
-       <NavLink href="/Labs" as={Link}>Labs</NavLink>
-     </NavItem>
-     <NavItem>
-       <NavLink href="/Labs/Lab1" as={Link}>Lab 1</NavLink>
-     </NavItem>
-     <NavItem>
-       <NavLink href="/Labs/Lab2" as={Link}>Lab 2</NavLink>
-     </NavItem>
-     <NavItem>
-       <NavLink href="/Labs/Lab3" as={Link}>Lab 3</NavLink>
-     </NavItem>
-     <NavItem>
-        <NavLink href="/Labs/Lab4" as={Link}>Lab 4</NavLink>
-     </NavItem>
-     <NavItem>
-        <NavLink href="/Labs/Lab5" as={Link}>Lab 5</NavLink>
-     </NavItem>
-     <NavItem>
-       <NavLink href="/" as={Link}>Kambaz</NavLink>
-     </NavItem>
-     <NavItem>
-       <NavLink href="https://github.com/ShamyaHaria">GitHub</NavLink>
-     </NavItem>
-   </Nav>
-);
+  return (
+    <Nav variant="pills" className="flex-column">
+      <NavItem>
+        <Link href="/Labs" className="nav-link">Labs</Link>
+      </NavItem>
+      <NavItem>
+        <Link href="/Labs/Lab1" className="nav-link">Lab 1</Link>
+      </NavItem>
+      <NavItem>
+        <Link href="/Labs/Lab2" className="nav-link">Lab 2</Link>
+      </NavItem>
+      <NavItem>
+        <Link href="/Labs/Lab3" className="nav-link">Lab 3</Link>
+      </NavItem>
+      <NavItem>
+        <Link href="/Labs/Lab4" className="nav-link">Lab 4</Link>
+      </NavItem>
+      <NavItem>
+        <Link href="/Labs/Lab5" className="nav-link">Lab 5</Link>
+      </NavItem>
+      <NavItem>
+        <Link href="/" className="nav-link">Kambaz</Link>
+      </NavItem>
+      <NavItem>
+        <a href="https://github.com/ShamyaHaria" className="nav-link" target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>
+      </NavItem>
+    </Nav>
+  );
 }
